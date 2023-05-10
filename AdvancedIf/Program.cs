2b98a0d2b2eb72ec225c45306b853daa5319b9ca
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-10 00:07:04 -0600

    Add project files.

---
+++ AdvancedIf/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("What is your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string lastName = Console.ReadLine();
+
+if (firstName.ToLower() == "mohammad" && lastName.ToLower() == "jabir")
+{
+    Console.WriteLine("Hello Mr. Jabir");
+}
